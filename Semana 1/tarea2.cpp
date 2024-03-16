0d6d294e9ae1984ae39039c1c882ecd65f349b5b
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-03-16 14:14:50 -0500

    Tarea2.cpp

--- Semana 1/tarea2.cpp
+++ Semana 1/tarea2.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+using namespace std;
 
 bool busq_bin(int *ini, int *fin, int *&pos, int key) {
-    if (ini >= fin) {
-        std::cout << "False" << std::endl; // Imprime "false" si el rango es vacío
+    if (ini >= fin) 
+    {
+        cout << "False" << endl; // Imprime "false" si el rango es vacío
         return false;
     }
 
     int *inicio = ini;
     int *final = fin - 1; // Ajustamos el puntero final para que apunte al último elemento del rango
 
-    for (int *p = inicio; inicio < final; ) {
+    for (int *p = inicio; inicio < final; ) 
+    {
         int *mid = inicio + (final - inicio) / 2;
         if (*mid == key) {
             pos = mid;
-            std::cout << "True\nPosición: " << (mid - ini) << std::endl; // Imprime "true" y la posición del elemento
+            cout << "True\nPosición: " << (mid - ini) << endl; // Imprime "true" y la posición del elemento
             return true;
         } else if (*mid < key) {
             inicio = mid + 1;
@@ -22,21 +25,23 @@ bool busq_bin(int *ini, int *fin, int *&pos, int key) {
         }
     }
 
-    if (*inicio == key) {
+    if (*inicio == key) 
+    {
         pos = inicio;
-        std::cout << "True\nPosición: " << (inicio - ini) << std::endl; // Imprime "true" y la posición del elemento
+        cout << "True\nPosición: " << (inicio - ini) << endl; // Imprime "true" y la posición del elemento
         return true;
     }
 
-    std::cout << "False" << std::endl; // Imprime "false"
+    cout << "False" << endl; // Imprime "false"
     return false;
 }
 
-int main() {
+int main() 
+{
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int key;
-    std::cout << "Ingrese el elemento a buscar: ";
-    std::cin >> key;
+    cout << "Ingrese el elemento a buscar: ";
+    cin >> key;
 
     int *pos;
     busq_bin(arr, arr + sizeof(arr) / sizeof(arr[0]), pos, key);
