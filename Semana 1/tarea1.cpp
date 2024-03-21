ab57ef1e98198ed2ed16d191a5b71128d92abf69
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-03-21 18:24:14 -0500

    Guardando cambios locales en tarea1.cpp

--- Semana 1/tarea1.cpp
+++ Semana 1/tarea1.cpp
@@ -1,14 +1,25 @@
-// 
+/*
+*/
 
+#include <iostream>
+using namespace std;
 
-class pica
+class piLa
 {
     int elem[10];
     int *TOP = NULL;
     bool push(int a);
     bool pop(int &a);
-
 };
+
+bool pila::push(int a)
+{
+    if (TOP - elem < 10)
+    {
+        *(+TOP) = a;
+        
+    }
+}
 
 int main()
 {
