e75f075ee3f6a2aadae60180ced15b084a69d606
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-04-04 21:23:28 -0500

    Quicksort tiempo

---
+++ Semana_4/sort.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+void swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int* particion(int* low, int* high){
+    int pivote = *high;
+    int* i = low - 1;
+    
+    for(int* j = low; j <= high; j++){
+        if(*j < pivote){
+            i++;
+            swap(i,j);
+        }
+    }
+    swap(i+1,high);
+    return(i+1);
+}
+
+void quick_sort(int* low, int*high){
+    if(low < high){
+        int *pi=particion(low,high);
+        quick_sort(low,pi-1);
+        quick_sort(pi+1,high);
+    }
+}
+
+int main(){
+    
+    auto start = chrono::high_resolution_clock::now();
+    
+    const int size = 1000;
+    int arr[size];
+
+    srand(time(nullptr));
+
+for(int i = 0; i < size; ++i){
+    arr[i] = rand() % 10000;
+}
+
+    cout << "\nArray desordenado:" << endl;
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << "\nArray ordenado:" << endl;
+    quick_sort(arr,arr + size - 1);
+
+    for (int i = 0; i < size; i++){
+        
+        cout << arr[i] << " ";
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << "\nTiempo:\nEl programa tardo " << duration.count() << " milisegundos en ejecutarse.\n";
+
+    return 0;
+}
