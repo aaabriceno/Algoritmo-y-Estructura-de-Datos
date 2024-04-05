3d12e692cdf2d8a51bb2469bd72b73ceface808a
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-04-05 13:24:42 -0500

    Quick sort Polomorfismo

---
+++ Semana_4/pol_sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+
+class sort{
+    private:
+
+    public:
+};
+
+class quick_sort:public sort{
+    
+};  
+
+int main(){
+    return 0;
+}
