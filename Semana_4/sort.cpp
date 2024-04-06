be8d6035094110a8c2b8f68c014b77ccd919b5c6
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-04-06 03:51:07 -0500

    Quick sort

--- Semana_4/sort.cpp
+++ Semana_4/sort.cpp
@@ -10,7 +10,7 @@ void swap(int* a, int* b){
     *b = temp;
 }
 
-int* particion(int* low, int* high){
+int* particion_asc(int* low, int* high){
     int pivote = *high;
     int* i = low - 1;
     
@@ -24,18 +24,40 @@ int* particion(int* low, int* high){
     return(i+1);
 }
 
+int* particion_desc(int* low, int* high) {
+    int pivot = *high;
+    int *i = low - 1;
 
-void quick_sort(int* low, int*high){
+    for (int *j = low; j <= high - 1; j++) {
+        if (*j >= pivot) {  
+            i++;
+            swap(i, j);
+        }
+    }
+    swap(i + 1, high);
+    return i + 1;
+}
+
+
+void quick_sort_asc(int* low, int*high){
     if(low < high){
-        int *pi=particion(low,high);
-        quick_sort(low,pi-1);
-        quick_sort(pi+1,high);
+        int *pi = particion_asc(low, high);
+        quick_sort_asc(low, pi - 1);
+        quick_sort_asc(pi + 1, high);
     }
 }
+
+void quick_sort_desc(int* low, int*high){
+    if(low < high){
+        int *pi = particion_desc(low, high);
+        quick_sort_desc(low, pi - 1);
+        quick_sort_desc(pi + 1, high);
+    }
+}
+
 int main(){
-    
     auto start = chrono::high_resolution_clock::now();
-    const int size = 100;
+    const int size = 50;
     int arr[size];
 
     srand(time(nullptr));
@@ -49,17 +71,23 @@ int main(){
         cout << arr[i] << " ";
     }
 
-    cout << "\nArray ordenado:" << endl;
-    quick_sort(arr,arr + size - 1);
+    cout << "\nArray ordenado ascendente:" << endl;
+    quick_sort_asc(arr, arr + size - 1);
 
     for (int i = 0; i < size; i++){
-        
+        cout << arr[i] << " ";
+    }
+
+    cout << "\nArray ordenado descendente:" << endl;
+    quick_sort_desc(arr, arr + size - 1);
+
+    for (int i = 0; i < size; i++){
         cout << arr[i] << " ";
     }
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
-    cout << "\nTiempo:\nEl programa tardo " << duration.count() << " milisegundos en ejecutarse.\n";
+    cout << "\nTiempo:\nEl programa tardó " << duration.count() << " milisegundos en ejecutarse.\n";
 
     return 0;
 }
