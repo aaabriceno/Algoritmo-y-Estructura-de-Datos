9bbc8c2351d8d64362093a08fdeeb2792eb4b60d
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-04-25 11:34:10 -0500

    Pila adaptador - conteiner

--- Semana_5/stack_adaptor/stack_cont_adaptor.cpp
+++ Semana_5/stack_adaptor/stack_cont_adaptor.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include "c_array.hpp"
+#include "c_vector.hpp"
+#include "c_deque.hpp"
+#include "c_linked_list.hpp"
 
 using namespace std;
+template <class Conteiner = Cvector>
 class stack_adaptor{
-
+    Conteiner conteiner;
 };
-
-class array{
-
-};
-int main();
