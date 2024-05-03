98661639bf3095f0f77e49ee1ec0a6adb580ef0c
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-05-02 20:36:33 -0500

    Cola en array prueba

---
+++ Semana_5/cola_prueba.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+class cola_en_array {
+public:
+    int arr[10];
+    int *head = nullptr;
+    int *tail = nullptr;
+    bool last_operation_push = false; // Booleano para indicar la última operación (push/pop)
+    bool push(int n);
+    bool pop(int &n);
+    void print();
+};
+
+bool cola_en_array::push(int n) {
+    if ((head == arr && tail == arr + 9) || (head == tail + 1)) {
+        // Cola llena
+        return false;
+    }
+    else if (tail == nullptr && head == nullptr) {
+        // Cola vacía
+        head = arr;
+        tail = arr;
+    }
+    else if (tail == arr + 9) {
+        tail = arr;
+    }
+    else {
+        tail++;
+    }
+    *tail = n;
+    last_operation_push = true; // La última operación fue un push
+    return true;
+}
+
+bool cola_en_array::pop(int &n) {
+    if (head == nullptr && tail == nullptr || head == tail+1) {
+        return false; // Cola vacía
+    }
+    n = *head;
+    if (head == arr + 9) {
+        head = arr + 10;
+    }
+    else {
+        head++;
+    }
+    if (head == tail + 1) {
+        // Último elemento eliminado
+        n = *head;
+        head = nullptr;
+        tail = nullptr;
+    }
+    last_operation_push = false; // La última operación fue un pop
+    return true;
+}
+
+void cola_en_array::print() {
+    if (head == nullptr || tail == nullptr) {
+        cout << "La cola está vacia." << endl;
+        return;
+    }
+    cout << "Elementos en la cola: ";
+    if (last_operation_push) {
+        int *current = tail;
+        do {
+            cout << *current << " ";
+            if (current == arr) {
+                current = arr + 9;
+            }
+            else {
+                current--;
+            }
+        } while (current != head);
+        cout << *current << endl;
+    } else {
+        int *current = head;
+        do {
+            cout << *current << " ";
+            if (current == arr + 9) {
+                current = arr;
+            }
+            else {
+                current++;
+            }
+        } while (current != tail);
+        cout << *current << endl;
+    }
+}
+
+int main() {
+    cola_en_array cola;
+
+    // Agregar elementos a la cola
+    cola.push(10);
+    cola.push(20);
+    cola.push(30);
+    cola.push(100);
+    cola.push(90);
+    cola.push(80);
+    cola.push(70);
+    cola.push(50);
+    cola.push(60);
+    cola.push(12);
+    //cola.push(13);
+    cola.print();
+  
+    int a;
+    cola.pop(a);
+    cola.pop(a);
+    cola.pop(a);
+    cola.pop(a);
+    cola.pop(a);
+    cola.pop(a); 
+    cola.pop(a);
+    cola.print();
+    
+    cola.push(111);
+    cola.push(112);
+    cola.push(113);
+    cola.push(114);
+    cola.push(115);
+    cola.print();
+    return 0;
+}
