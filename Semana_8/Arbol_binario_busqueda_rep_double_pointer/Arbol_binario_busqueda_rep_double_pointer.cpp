40bd47b7dc23a32492e861e0982937f5d8e87f7a
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-05-06 16:11:52 -0500

    Agregando el metodo remover para arbol binario de busqueada con repeticion

--- Semana_8/Arbol_binario_busqueda_rep_double_pointer/Arbol_binario_busqueda_rep_double_pointer.cpp
+++ Semana_8/Arbol_binario_busqueda_rep_double_pointer/Arbol_binario_busqueda_rep_double_pointer.cpp
@@ -22,20 +22,30 @@ public:
 
 private:
 	void inOrder(CbintreeNode* n);
+	CbintreeNode** reemplazar(CbintreeNode** p);
 	bool buscar(int x, CbintreeNode**& p);
+	bool buscar_remove(int x, CbintreeNode**& p);
 	bool bb;
 	CbintreeNode* root;
 };
-Cbintree::Cbintree() {bb = 0;root = 0;}
+Cbintree::Cbintree() { bb = 0; root = 0; }
 Cbintree::~Cbintree() {}
 
-bool Cbintree::buscar(int x, CbintreeNode**&p){
+bool Cbintree::buscar(int x, CbintreeNode**& p) {
 	p = &root;
 	while ((*p != 0) && (((*p)->dato != x) || (*p)->dato == x)) {
 		p = &((*p)->nodes[x > (*p)->dato]);
 	}
 	return (*p != 0);
 }
+bool Cbintree::buscar_remove(int x, CbintreeNode**& p) {
+	p = &root;
+	while ((*p != 0) && (((*p)->dato != x)	) {
+		p = &((*p)->nodes[x > (*p)->dato]);
+	}
+	return (*p != 0);
+}
+
 bool Cbintree::insertar(int x) {
 	CbintreeNode** p;
 	if (buscar(x, p)) {
@@ -54,11 +64,35 @@ bool Cbintree::insertar(int x) {
 	}
 	return 1;
 }
-/*
-bool Cbintree::remover(int x) {
-
+CbintreeNode** Cbintree::reemplazar(CbintreeNode** p){
+	CbintreeNode** q;
+	int b = bb;
+	q = &((*p)->nodes[b]);
+	while ((*q)->nodes[!b]) {
+		q = &((*q)->nodes[!b]);
+	}
+	bb = !bb;
+	return q;
 }
-*/
+
+bool Cbintree::remover(int x) {
+	CbintreeNode** p;
+	if (!buscar_remove(x,p) )return 0;
+	/*caso 2; si es que tiene dos hijos*/
+	if ((*p)->nodes[0] && (*p)->nodes[1]){
+		CbintreeNode** q = reemplazar(p);
+		(*p)->dato = (*q)->dato;
+		p = q;
+	}
+	//caso 1.1 - 1.2 si es que tiene un hijo izquierda o derecha
+		CbintreeNode* t = *p;
+
+		*p = (*p)->nodes[(*p)->nodes[0] == 0];
+		delete t;
+		(*p)->contador--;
+		return 1;
+}
+
 void Cbintree::inOrder(CbintreeNode* n) {
 	if (!n) return;
 	inOrder(n->nodes[0]);
@@ -86,6 +120,12 @@ int main() {
 	l.insertar(46);
 	l.insertar(47);
 	l.insertar(4);
+	l.insertar(3);
+	l.print();
+
+	l.remover(5);
+	l.remover(3);
+	l.remover(45);
 	l.print();
 
 	return 0;
