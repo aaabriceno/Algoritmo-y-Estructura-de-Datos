2342db8d770bf50d85c5361e0c864a1a73d1974f
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-05-13 21:41:06 -0500

    Reocrrido de arboles

---
+++ Semana_10/recorrido_arboles/recorrido_arboles.cpp
@@ -0,0 +1,47 @@
+/*Recorrido de arboles*/
+#include <iostream>
+using namespace std;
+/*
+	Cabe recalcar que se procesa en un stack, while(mientras la pila no este vacia se
+	procesa el tope, se utiliza recursividad, y se procesa el stack, bajo el control del usuario) 
+	Se debe trrabajar por estados para que se pueda recorrer el arbol de manera correcta
+-	Estado 0: Si el recorrido se hace por primera vez
+-	Estado 1: Si ya se bajo por izquierda y regreso al nodo 
+-	Estado 2: Si debe bajar por la derecha y regresa al nodo
+-	Estado 3: Que se debe sacar
+
+struct  cx{
+	cx* n;
+	int s;
+}
+std::stack<cx> s;
+
+std::pair <char,int> uu;
+uu first = 'c';
+uu second = 7;
+std::stack <std::pair<Node*, int>> s; <---- el stack esta basado en un deque :)
+s.push ({p,z});
+
+
+existe tambien --> std::tuple<int , char, float> t;
+*/
+struct Node {
+	int valor;
+	Node* nodes[2];
+	Node(int v) {
+		valor = v;
+		nodes[0] = nodes[1] = 0;
+	}
+};
+
+/*Esta funcion es un stack recursivo automatico a diferencia del caso de arriba*/
+void inorder(Node* n) { //metodo in orden tipico 
+	if (!n) return;
+	inorder(n->nodes[0]);
+	cout << n->valor;
+	inorder(n->nodes[1]);
+}
+
+int  main() {
+
+}
