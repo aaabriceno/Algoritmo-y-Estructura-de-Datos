0595835a2a74313964ce9a6c6d0568215b63ceec
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-07-06 06:00:09 -0500

    Grafos

--- Semana_15/Grafos/Grafos.cpp
+++ Semana_15/Grafos/Grafos.cpp
@@ -44,9 +44,47 @@ void Cgraph::InsertNode(char v) {
 	Cnode n(nodes.size(), v);
 	nodes.push_back(n);
 }
+void Cgraph::RemNode(char v) {
+	// Encontrar el nodo a eliminar
+	auto it = find_if(nodes.begin(), nodes.end(), [v](const Cnode& node) {
+		return node.valor == v;
+		});
 
-void Cgraph::RemEdge{
+	if (it != nodes.end()) {
+		int id = it->id_node;
 
+		// Eliminar todas las aristas que apuntan al nodo
+		for (auto& node : nodes) {
+			node.edges.remove_if([id](const Cedge& edge) {
+				return edge.id_node == id;
+				});
+		}
+
+		// Eliminar el nodo
+		nodes.erase(it);
+
+		// Actualizar los IDs de los nodos restantes
+		for (size_t i = 0; i < nodes.size(); ++i) {
+			nodes[i].id_node = i;
+		}
+
+		// Actualizar los IDs de las aristas
+		for (auto& node : nodes) {
+			for (auto& edge : node.edges) {
+				if (edge.id_node > id) {
+					edge.id_node--;
+				}
+			}
+		}
+	}
+}
+
+void Cgraph::RemEdge(int idn1, int idn2, int v) {
+	if (idn1 < nodes.size()) {
+		nodes[idn1].edges.remove_if([idn2, v](const Cedge& edge) {
+			return edge.id_node == idn2 && edge.valor == v;
+			});
+	}
 }
 
 void Cgraph::InsertEdge(int idn1, int idn2, int v) {
@@ -78,6 +116,15 @@ int main()
 	g.InsertEdge(1, 3, 4); g.InsertEdge(1, 2, 2); //B
 	g.InsertEdge(2, 3, 1); g.InsertEdge(2, 4, 5); //C
 	
+	cout << "Grafo original:" << endl;
+	g.print();
+
+	g.RemNode('C');
+	cout << "\nGrafo despues de eliminar el nodo 'C':" << endl;
+	g.print();
+
+	g.RemEdge(0, 1, 3);
+	cout << "\nGrafo despues de eliminar la arista de 'A' a 'B' con valor 3:" << endl;
 	g.print();
 	return 0;
 }
